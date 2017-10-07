f9c43349b8fe27e037d79a8463bbe7a615a27ebb
sunqf <sunqf365@yahoo.co.jp>
2017-10-07 17:41:35 +0900

    update

--- MVCApplication/Models/AccountViewModels.cs
+++ MVCApplication/Models/AccountViewModels.cs
@@ -70,7 +70,7 @@ namespace MVCApplication.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个 字符。", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "密码")]
         public string Password { get; set; }
